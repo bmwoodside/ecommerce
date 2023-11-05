f708fa488eb854bd0776b0a9a35732fd33274d33
Matt Woodside <bmwoodside@live.com>
2023-11-05 17:57:35 -0500

    updated Items.jsx to call the dnd5eApi graphQL API (WIP still); add tons of junk to Item.jsx - it was a test-bed for Items, but some of the info is useful, so it's in the file still...; broke Items.jsx from rendering, commented bugs for now;

--- ecommerceApp/src/components/Item.jsx
+++ ecommerceApp/src/components/Item.jsx
@@ -1,8 +1,120 @@
-
-
+import { useState } from "react"
 
 const Item = (props) => {
 
+    const [itemsList, setItemsList] = useState();
+    
+    const GetAllEquipmentFromGraphQL = () => {
+        const url = 'https://www.dnd5eapi.co/graphql';
+    
+        const query = `
+            query Equipments {
+                equipments {
+                    ... on Weapon {
+                        index
+                        name
+                        desc
+                        weight
+                        weapon_range
+                        special
+                        cost {
+                            quantity
+                            unit
+                        }
+                        equipment_category {
+                            index
+                            name
+                        }
+                        damage {
+                            damage_dice
+                            damage_type {
+                                index
+                                name
+                                desc
+                            }
+                        }
+                        range {
+                            normal
+                            long
+                        }
+                    }
+                    ... on Armor {
+                        index
+                        name
+                        desc
+                        weight
+                        str_minimum
+                        stealth_disadvantage
+                        cost {
+                            quantity
+                            unit
+                        }
+                        equipment_category {
+                            index
+                            name
+                        }
+                        armor_class {
+                            base
+                            dex_bonus
+                            max_bonus
+                        }
+                        armor_category {
+                            index
+                            name
+                        }
+                    }
+                }
+            }
+            
+        `;
+
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'data': '{"query":"query { __typename }"}'
+            },
+            body: JSON.stringify({query})
+        };
+    
+        const request = new Request(url, options);
+
+        fetch(request)
+            .then(res => {
+                console.log("res", res);
+                return res.json();
+            })
+            .then(data => {
+                console.log("data", data);
+                console.log("equip", data.data.equipments);
+                setItemsList(data.data.equipments);
+            })
+            .catch(err => console.log(err));
+    };
+
+    // had to move this out of the return block so that i could properly comment it out without breaking the app
+    // easy quick-reference for the render from the data element/query:
+    // <hr />
+    // {
+    //     itemsList
+    //     ? (
+    //         <>
+    //         {/* placeholder weapon */}
+    //         <h1>{itemsList[0].index}</h1>
+    //         <h1>{itemsList[0].damage.damage_dice}</h1>
+    //         <h1>{itemsList[0].damage.damage_type.index}</h1>
+    //         <h1>{itemsList[0].range.normal === 5 ? "melee" : itemsList[0].range.normal}</h1>
+
+    //         <hr />
+
+    //         {/* placeholder armor */}
+    //         <h1>{itemsList[39].index}</h1>
+    //         <h1>{itemsList[39].armor_class.base}</h1>
+    //         <h1>{itemsList[39].armor_category.name}</h1>
+    //         <h1>{itemsList[39].armor_class.dex_bonus === true ? "yes" : "no"}</h1>
+    //         </>
+    //     ) : null
+    // }
 
 
     return (
@@ -13,6 +125,30 @@ const Item = (props) => {
             obj[Other_items_in_order_of_render_priority] ? obj[info] : null (figure out how to ternary without the "else" condition) */}
 
             <h1 className="item-name">Item Placeholder</h1>
+
+            {/* this whole page turned into a test-page to make the "Items" component render */}
+
+            <button className="rounded-full bg-indigo-400" onClick={GetAllEquipmentFromGraphQL}>fetch data</button>
+
+            <hr />
+            {
+                itemsList
+                ? (
+                    itemsList.map((oneItem, i) => (
+                        oneItem.index
+                        ? (
+                            <div>
+                                <h1 key={i}>{oneItem.index}</h1>
+
+                                {/* this doesn't work obv - has nested children for the right info - adding the correct info breaks this shit for some reason */}
+                                <h1>{oneItem.damage_type}</h1>
+                                
+                                <h1>{oneItem.equipment_category.index}</h1>
+                            </div>
+                        ) : null
+                    ))
+                ) : null
+            }
 
         </div>
     )
--- ecommerceApp/src/components/Items.jsx
+++ ecommerceApp/src/components/Items.jsx
@@ -1,18 +1,99 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 
 const Items = (props) => {
     // would be nice if i could figure out how to populate the images as well. stretch goal - **Future Feature**
 
-    // this will need to be a getAll useEffect/useMemo/something to populate initial state (pending backend creation)
-    let tempItemsArr = [
-        {"desc":[],"special":[],"index":"club1","name":"Club1","equipment_category":{"index":"weapon","name":"Weapon","url":"/api/equipment-categories/weapon"},"weapon_category":"Simple","weapon_range":"Melee","category_range":"Simple Melee","cost":{"quantity":1,"unit":"sp"},"damage":{"damage_dice":"1d4","damage_type":{"index":"bludgeoning","name":"Bludgeoning","url":"/api/damage-types/bludgeoning"}},"range":{"normal":5},"weight":2,"properties":[{"index":"light","name":"Light","url":"/api/weapon-properties/light"},{"index":"monk","name":"Monk","url":"/api/weapon-properties/monk"}],"url":"/api/equipment/club","contents":[]},
+    const [itemsList, setItemsList] = useState();
+    
+    const GetAllEquipmentFromGraphQL = () => {
+        const url = 'https://www.dnd5eapi.co/graphql';
+    
+        const query = `
+            query Equipments {
+                equipments {
+                    ... on Weapon {
+                        index
+                        name
+                        desc
+                        weight
+                        weapon_range
+                        special
+                        cost {
+                            quantity
+                            unit
+                        }
+                        equipment_category {
+                            index
+                            name
+                        }
+                        damage {
+                            damage_dice
+                            damage_type {
+                                index
+                                name
+                                desc
+                            }
+                        }
+                        range {
+                            normal
+                            long
+                        }
+                    }
+                    ... on Armor {
+                        index
+                        name
+                        desc
+                        weight
+                        str_minimum
+                        stealth_disadvantage
+                        cost {
+                            quantity
+                            unit
+                        }
+                        equipment_category {
+                            index
+                            name
+                        }
+                        armor_class {
+                            base
+                            dex_bonus
+                            max_bonus
+                        }
+                        armor_category {
+                            index
+                            name
+                        }
+                    }
+                }
+            }
+            
+        `;
 
-        {"desc":[],"special":[],"index":"club2","name":"Club2","equipment_category":{"index":"weapon","name":"Weapon","url":"/api/equipment-categories/weapon"},"weapon_category":"Simple","weapon_range":"Melee","category_range":"Simple Melee","cost":{"quantity":1,"unit":"sp"},"damage":{"damage_dice":"1d4","damage_type":{"index":"bludgeoning","name":"Bludgeoning","url":"/api/damage-types/bludgeoning"}},"range":{"normal":5},"weight":2,"properties":[{"index":"light","name":"Light","url":"/api/weapon-properties/light"},{"index":"monk","name":"Monk","url":"/api/weapon-properties/monk"}],"url":"/api/equipment/club","contents":[]},
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'data': '{"query":"query { __typename }"}'
+            },
+            body: JSON.stringify({query})
+        };
+    
+        const request = new Request(url, options);
 
-        {"desc":[],"special":[],"index":"padded-armor","name":"Padded Armor","equipment_category":{"index":"armor","name":"Armor","url":"/api/equipment-categories/armor"},"armor_category":"Light","armor_class":{"base":11,"dex_bonus":true},"str_minimum":0,"stealth_disadvantage":true,"weight":8,"cost":{"quantity":5,"unit":"gp"},"url":"/api/equipment/padded-armor","contents":[],"properties":[]},
-
-    ]
+        fetch(request)
+            .then(res => {
+                console.log("res", res);
+                return res.json();
+            })
+            .then(data => {
+                console.log("data", data);
+                console.log("equip", data.data.equipments);
+                setItemsList(data.data.equipments);
+            })
+            .catch(err => console.log(err));
+    };
 
     const addToCart = (e) => {
         e.preventDefault();
@@ -23,75 +104,78 @@ const Items = (props) => {
         console.log("added something to the cart!");
     }
 
-    // {"desc":[],"special":[],"index":"club1","name":"Club1","equipment_category":{"index":"weapon","name":"Weapon","url":"/api/equipment-categories/weapon"},"weapon_category":"Simple","weapon_range":"Melee","category_range":"Simple Melee","cost":{"quantity":1,"unit":"sp"},"damage":{"damage_dice":"1d4","damage_type":{"index":"bludgeoning","name":"Bludgeoning","url":"/api/damage-types/bludgeoning"}},"range":{"normal":5},"weight":2,"properties":[{"index":"light","name":"Light","url":"/api/weapon-properties/light"},{"index":"monk","name":"Monk","url":"/api/weapon-properties/monk"}],"url":"/api/equipment/club","contents":[]}
-
-    // {"desc":[],"special":[],"index":"padded-armor","name":"Padded Armor","equipment_category":{"index":"armor","name":"Armor","url":"/api/equipment-categories/armor"},"armor_category":"Light","armor_class":{"base":11,"dex_bonus":true},"str_minimum":0,"stealth_disadvantage":true,"weight":8,"cost":{"quantity":5,"unit":"gp"},"url":"/api/equipment/padded-armor","contents":[],"properties":[]}
-
-
-
     return (
         <div className="items-component-container bg-white">
+
+            {/* replace me with a useEffect/useMemo or something. */}
+            <button onClick={GetAllEquipmentFromGraphQL}>fetch data</button>
+
             <div className="mx-auto max-w-7x1 overflow-hidden sm:px-6 lg:px-8 border-solid border-4 border-red-600">
                 <h2 className="sr-only">Items List</h2>
-                {/* <h1>placeholder1</h1> */}
 
                 <div className="-mx-px grid grid-cols-2 border-1 border-gray-200 sm:mx-0 md:grid-cols-3 lg:grid-cols-4 items-list-container">
-                    {/* <h1>placeholder2</h1> */}
-
-                    {tempItemsArr.map((oneItem, i) => 
-                        <div key={i} className="group relative border-b border-r border-gray-200 p-4 sm:p-6">
-                            {/* <h1>placeholder3</h1> */}
-
-                            <div className="aspect-h-1 aspect-w-1 overflow-hidden rounded-lg bg-gray-200 group-hover:opacity-75">
-                                <img
-                                    src="placeholder"
-                                    alt="placeholder"
-                                    className="h-full w-full object-cover object-center"
-                                />
-                            </div>
-
-                            <div className="pb-4 pt-10 text-center flex flex-1 flex-col space-y-2 p-4">
-                                <h3 className="text-sm font-medium text-gray-900">
-                                    <Link to={`/items/${oneItem.name}`}>
-                                        <span aria-hidden="true" className="absolute inset-0" />
-                                        {oneItem.name}
-                                    </Link>
-                                </h3>
-
-                                {oneItem.equipment_category.index == "weapon"
-                                    ? (<div className="weapon-info">
-                                        <p className="text-sm text-gray-500">Damage: {oneItem.damage.damage_dice} {oneItem.damage.damage_type.index}</p>
-                                        <p className="text-sm text-gray-500">Range: {oneItem.range.normal === 5 ? "Melee" : oneItem.range.normal}</p>
-
-                                        {/* See how my styling is different than the other button below?? -- I'm a placeholder button so you don't forget -- remove me */}
-                                        <button type="button" className="rounded-full bg-indigo-600 px-3 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-                                            Add To Cart
-                                        </button>
-                                        {/* Remove the button element above this line (after fixing styles) -- don't forget! */}
-
-                                    </div>)
-                                : oneItem.equipment_category.index == "armor"
-                                    ? (<div className="armor-info">
-                                        <p className="text-sm text-gray-500">AC: {oneItem.armor_class.base}</p>
-                                        <p className="text-sm text-gray-500">Armor Type: {oneItem.armor_category}</p>
-                                        <p className="text-sm text-gray-500">+Dex Bonus: {oneItem.armor_class.dex_bonus == true ? "Yes" : "No"}</p>
-                                    </div>)
-                                    : (<em>Error</em>)
-                                }
-
-                                <div className="flex flex-1 flex-col justify-end">
-                                    {/* <p className="text-sm italic text-gray-500">{product.options}</p> */}
-                                    <p className="text-sm italic text-gray-500">placeholder options</p>
-                                    <p className="text-base font-medium test-gray-900">{oneItem.cost.quantity} {oneItem.cost.unit}</p>
-
-                                    {/* styling is jacked up - this is full div width but above is only partial... pick a size */}
-                                    <button type="button" className="rounded-full bg-indigo-600 px-3 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-                                        Add To Cart
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                    )}
+                    {
+                        itemsList
+                        ? (
+                            itemsList.map((oneItem, i) => (
+                                oneItem.index
+                                ? (
+                                    <div key={i} className="group relative border-b border-r border-gray-200 p-4 sm:p-6">
+            
+                                        <div className="aspect-h-1 aspect-w-1 overflow-hidden rounded-lg bg-gray-200 group-hover:opacity-75">
+                                            <img
+                                                src="placeholder"
+                                                alt="placeholder"
+                                                className="h-full w-full object-cover object-center"
+                                            />
+                                        </div>
+            
+                                        <div className="pb-4 pt-10 text-center flex flex-1 flex-col space-y-2 p-4">
+                                            <h3 className="text-sm font-medium text-gray-900">
+                                                <Link to={`/items/${oneItem.name}`}>
+                                                    <span aria-hidden="true" className="absolute inset-0" />
+                                                    {oneItem.name}
+                                                </Link>
+                                            </h3>
+            
+                                            {oneItem.equipment_category.index == "weapon"
+                                                ? (<div className="weapon-info">
+                                                    {/* the commented line below about damage_dice is erroring for what feels like an issue with 
+                                                    the equipment_category condition checking - seems to not differentiate correctly. */}
+                                                    {/* <p className="text-sm text-gray-500">Damage: {oneItem.damage.damage_dice} {oneItem.damage.damage_type.index}</p> */}
+                                                    <p className="text-sm text-gray-500">Range: {oneItem.range.normal === 5 ? "Melee" : oneItem.range.normal}</p>
+            
+                                                    {/* See how my styling is different than the other button below?? -- I'm a placeholder button so you don't forget -- remove me */}
+                                                    <button type="button" className="rounded-full bg-indigo-600 px-3 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                                                        Add To Cart
+                                                    </button>
+                                                    {/* Remove the button element above this line (after fixing styles) -- don't forget! */}
+            
+                                                </div>)
+                                            : oneItem.equipment_category.index == "armor"
+                                                ? (<div className="armor-info">
+                                                    <p className="text-sm text-gray-500">AC: {oneItem.armor_class.base}</p>
+                                                    <p className="text-sm text-gray-500">Armor Type: {oneItem.armor_category.name}</p>
+                                                    <p className="text-sm text-gray-500">+Dex Bonus: {oneItem.armor_class.dex_bonus == true ? "Yes" : "No"}</p>
+                                                </div>)
+                                                : (<em>Error</em>)
+                                            }
+            
+                                            <div className="flex flex-1 flex-col justify-end">
+                                                <p className="text-sm italic text-gray-500">placeholder options</p>
+                                                <p className="text-base font-medium test-gray-900">{oneItem.cost.quantity} {oneItem.cost.unit}</p>
+            
+                                                {/* styling is jacked up - this is full div width but above is only partial... pick a size */}
+                                                <button type="button" className="rounded-full bg-indigo-600 px-3 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                                                    Add To Cart
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                ) : null
+                            ))
+                        ) : "Click button to load data.. (change me with the useEffect addition to 'loading..')" 
+                    }
                 </div>
             </div>
         </div>
